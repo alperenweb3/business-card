7ff37e9d31270fd398f3813c7ccb096706e2ea77
Alperen Özkan <alperenweb3@gmail.com>
2023-07-17 12:29:19 +0200

    vercel analytics integrated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
 
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 function App() {
 	return (
 		<div className="main">
